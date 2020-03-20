765c224e1e90f035ffc650eb0e7b2452f1568cb9
ultim <ultim@175.115.5.176>
2020-03-20 10:44:35 +0900

    PRODUCT 페이지 추가

---
+++ src/main/webapp/WEB-INF/views/s2/s2_9.php
@@ -0,0 +1,341 @@
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<!-- <meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1.1">
+ -->
+<title>chemsolution</title>
+
+<link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/nanumgothic.css" />
+<link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/notosanskr.css" />
+</head>
+<link rel="stylesheet" href="http://chemsolution.net/css/jquery-ui.css">
+<link rel="stylesheet" href="http://chemsolution.net/css/jquery.bxslider.css">
+<link rel="stylesheet" href="http://chemsolution.net/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/reset.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/layout.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/main.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/sub.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/cnt.css">
+<link rel="stylesheet" href="http://chemsolution.net/theme/chemsolution/css/default/bbs.css">
+<link rel="stylesheet" href="http://chemsolution.net/css/etc.css">
+
+<!-- 네이버 메타태그 -->
+<!-- <meta name="naver-site-verification" content="" /> -->
+<meta name="naver-site-verification" content="bd14e7b02947541b4180283c35fef82f49cbbe58"/>
+
+<!-- 일반 메타태그 -->
+<!-- <meta name="description" content="" /> -->
+<meta name="description" content="용제,가소제부산물,윤활유,합성수지,연마제">
+<meta name="google-site-verification" content="3SrFVjVVVis-4IHnIQKO3WyeSGkc6DqUGmQfoOq9aZ8" />
+<link rel="canonical" href="" />
+
+<!-- SNS관련 메타태그 -->
+<meta property="og:type" content="website" />
+<meta property="og:title" content="chemsolution" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="" />
+<meta property="og:url" content="" />
+
+<!--[if lte IE 8]>
+	<script src="http://chemsolution.net/js/html5.js"></script>
+<![endif]-->
+<script>
+	// 자바스크립트에서 사용하는 전역변수 선언
+	var g5_url       = "http://chemsolution.net";
+	var g5_bbs_url   = "http://chemsolution.net/bbs";
+	var g5_is_member = "";
+	var g5_is_admin  = "";
+	var g5_is_mobile = "";
+	var g5_bo_table  = "";
+	var g5_sca       = "";
+	var g5_editor    = "";
+	var g5_cookie_domain = "";
+</script>
+<script src="http://chemsolution.net/js/jquery-1.11.1.js"></script>
+<script src="http://chemsolution.net/js/jquery.menu.js?ver=161020"></script>
+<script src="http://chemsolution.net/js/common.js?ver=161020"></script>
+<script src="http://chemsolution.net/js/wrest.js?ver=161020"></script>
+<script src="http://chemsolution.net/js/jquery-ui.js"></script>
+<script src="http://chemsolution.net/js/jquery.bxslider.js"></script>
+<script src="http://chemsolution.net/js/printThis.js"></script>
+<script src="http://chemsolution.net/theme/chemsolution/js/index.js"></script>
+</head>
+<body>
+<header id="header">
+	<div id="header_box">
+		<div id="logo">
+			<h1>
+				<a href="http://chemsolution.net">
+					<img src="http://chemsolution.net/theme/chemsolution/images/main/main_logo.png">
+			</h1>
+				</a>
+		</div>
+		<nav class="header_nav">
+			<ul>
+				<li><a href="http://chemsolution.net/s1/s1_1.php">ABOUT US</a>
+					<ul>
+					</ul>
+				</li>
+				<li><a href="http://chemsolution.net/s2/s2_1.php">PRODUCTS</a>
+						<ul>
+						<li><a href="http://chemsolution.net/s2/s2_1.php">AROMATIC SOLVENT</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_2_1.php">ALIPHATIC HYDROCARBON</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_3.php">PETROLEUM SOLVENT</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_4.php">LUBE BASE OIL</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_5.php">SYNTHETIC RESIN</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_6.php">ADDITIVE</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_7.php">LUBRICANT ADDITVE</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_8.php">ELECTRONIC SOLVENT</a></li>
+						<li><a href="http://chemsolution.net/s2/s2_9.php">ABRASIVE</a></li>
+					</ul>
+				</li>
+				<li><a href="http://chemsolution.net/s3/s3_1.php">CONTACT US</a>
+					<ul>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+
+	</div><!-- header_box -->
+</header>
+
+<div id="sub_visual"></div>
+<div id="sub_layout">
+	<div id="ln_area">
+		<h2 class="h2_label">PRODUCTS</h2>
+		<ul class="ln">
+							<li class=""><a href="http://chemsolution.net/s2/s2_1.php">AROMATIC SOLVENT</a></li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_2_1.php">ALIPHATIC HYDROCARBON</a>
+					<ul>
+						<li class="current"><a href="http://chemsolution.net/s2/s2_2_1.php">- ISOPARAFFINIC HYDROCARBON</a></li>
+						<li class=""><a href="http://chemsolution.net/s2/s2_2_2.php">- DEAROMATIC HYDROCARBON</a></li>
+						<li class=""><a href="http://chemsolution.net/s2/s2_2_3.php">- NAPHTHENIC HYDROCARBON</a></li>
+					</ul>
+				</li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_3.php">PETROLEUM SOLVENT</a></li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_4.php">LUBE BASE OIL</a></li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_5.php">SYNTHETIC RESIN</a></li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_6.php">ADDITIVE</a>
+				</li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_7.php">LUBRICANT ADDITVE</a></li>
+				<li class=""><a href="http://chemsolution.net/s2/s2_8.php">ELECTRONIC SOLVENT</a></li>
+				<li class="current"><a href="http://chemsolution.net/s2/s2_9.php">ABRASIVE</a></li>
+					</ul>
+	</div>
+	<div id="sub_cnt">
+		<div class="feedback">
+					<h3 class="h3_label">CM BALL</h3>
+									<div class="process">
+				<span class="home">HOME</span>
+				<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt="" /></span>
+				<span class="path">PRODUCTS</span>
+			
+								<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt="" /></span>
+				<span class="path">ABRASIVE</span>
+				<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt="" /></span>
+				<span class="path">CM BALL</span>
+			</div>	
+		</div>
+<article class="s0201 clear">
+	<div id="s0201_top">
+	 <p>CM Ball(Chem Metal Ball) is a rolled steel slag that recycles steelmaking slag, a by-product of the steelworks. CM Ball has excellent physical properties such as spherical shape, high strength and low abrasive rate. Using these characteristics, the excellent performance was proven in use of repair and reinforcing material, abrasive blasting material, roof material, weight material, non-slip material, etc.</p>
+	</div>
+	<div class="s0201_name">
+		<ul>
+			<li>Composition(%)</li>
+			<li></li>
+		</ul>
+	</div>
+		<table class="table2_9">
+			<thead>
+				<tr>
+					 <td>SiO<small>2</small></td>
+					 <td>Al<small>2</small>O<small>3</small></td>
+					 <td>MnO</td>
+					 <td>CaO</td>
+					 <td>MgO</td>
+					 <td>T-Fe</td>
+					 <td>Fe<small>2</small>O<small>3</small></td>
+					 <td>P<small>2</small>O<small>5</small></td>
+					 <td>Cr<small>2</small>O<small>3</small></td>
+					 <td>TiO<small>2</small></td>
+					 <td>Na<small>2</small>O</td>
+					 <td>K<small>2</small>O<small></td>
+				</tr>
+			</thead>
+				<tr>
+					<td>11.84</td>
+					<td>2.19</td>
+					<td>1.66</td>
+					<td>42.62</td>
+					<td>8.37</td>
+					<td>21.09</td>
+					<td></td>
+					<td></td>
+					<td>2.41</td>
+					<td>0.21</td>
+					<td>0.42</td>
+					<td></td>
+				</tr>
+	</table>
+
+	<div class="s0201_name margintop">
+		<ul>
+			<li class="mt">Size(mm)</li>
+			<li></li>
+		</ul>
+	</div>
+	
+	<div id="size_wrap">
+		<ul>
+			<li>
+				<img src="http://chemsolution.net/theme/chemsolution/images/sub/sub2_9_1.jpg">
+			<p>~0.6</p>
+				</li>
+			<li><img src="http://chemsolution.net/theme/chemsolution/images/sub/sub2_9_2.jpg">
+				<p>0.6~1.0</p>
+			</li>
+			<li>
+				<img src="http://chemsolution.net/theme/chemsolution/images/sub/sub2_9_3.jpg">
+				<p>0.6~2.0</p>
+			</li>
+			<li>
+				<img src="http://chemsolution.net/theme/chemsolution/images/sub/sub2_9_4.jpg">
+				<p>1.0~2.0</p>
+			</li>
+		</ul>
+	</div>
+
+	<div class="features">
+			<div class="feat_t">
+				<span>Application</span>
+			</div>
+			
+		<table class="table2_9_2">
+				<thead>
+					<tr>
+						 <td>Application</td>
+						 <td>Advantage</td>
+					</tr>
+				</thead>
+					<tr>
+						<td class="color1">Repair & Reinforcing Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Save pastes(cement, polymer) with high strength and low absorption rate</li>
+								<li class="left_cel">Eases drying shrinkage with the spherical particles</li>
+								<li class="left_cel">Excellent self-compacting with high fluidity</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Abrasive Blasting Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Lower price than rival goods</li>
+								<li class="left_cel">Good in tensity of illumination</li>
+								<li class="left_cel">Excellent hardness(Mohs hardness 7 or higher)</li>
+								<li class="left_cel">Less dust than Silica and Garnet </li>
+								<li class="left_cel">Reuse available(2〜4 times)</li>
+								<li class="left_cel">Waste after blasting can be classified as non-scheduled waste</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Roof Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">High class texture due to glossy surface</li>
+								<li class="left_cel">Excellent compressive strength, wear resistance, wind resistance, etc.</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Weight Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Effective in use of weight material due to high specific gravity</li>
+								<li class="left_cel">Excellent strength, less expensive</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Manhole Repair Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Improve compressive strength  </li>
+								<li class="left_cel">Reduce the resin consumption</li>
+								<li class="left_cel">Road surface goes with the color of CM Ball(school zone)</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Non-Slip Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Produce diverse products through coloring(school zone)</li>
+								<li class="left_cel">Less product unit cost with direct product process and construction</li>
+								<li class="left_cel">Longer product lifespan after construction due to excellent wear resistance</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td class="color1">Poly-Concrete Material</td>
+						<td>
+							<ul>
+								<li class="left_cel">Save the volume of  Polymer </li>
+								<li class="left_cel">Increase fluidity for manufacturing</li>
+								<li class="left_cel">Improve compressive strength</li>
+							</ul>
+						</td>
+					</tr>
+		</table>
+	</div>
+</article>
+
+
+
+
+
+	</div>
+</div>
+<footer id="footer_wrap">
+	<div id="footer" class="center">
+		<p>Company Name : ChemSolution Co., Ltd.</p>
+		<p>Address : 12, Seongsan-ro, Yeosu-si, Jeollanam-do, Republic of Korea (59642)</p>
+		<p>Tel : +82-61-686-8815 Fax : +82-61-686-8814</p>
+		<span>Copyright &copy; 2017 chemsolution Co., Ltd All rights reserved.</span>
+	</div>
+</footer>
+<div class="layer_popup">
+	<iframe src="" name="iFrame_area"></iframe>
+</div>	<a href="http://chemsolution.net/s2/s2_9.php?device=mobile" id="device_change">모바일 버전으로 보기</a>
+<script>
+	$(function() {
+		// 폰트 리사이즈 쿠키있으면 실행
+		font_resize("container", get_cookie("ck_font_resize_rmv_class"), get_cookie("ck_font_resize_add_class"));
+	});
+</script>
+
+
+<!-- ie6,7에서 사이드뷰가 게시판 목록에서 아래 사이드뷰에 가려지는 현상 수정 -->
+<!--[if lte IE 7]>
+<script>
+$(function() {
+    var $sv_use = $(".sv_use");
+    var count = $sv_use.length;
+
+    $sv_use.each(function() {
+        $(this).css("z-index", count);
+        $(this).css("position", "relative");
+        count = count - 1;
+    });
+});
+</script>
+<![endif]-->
+
+</body>
+</html>
